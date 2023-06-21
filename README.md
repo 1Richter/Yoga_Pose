49b40f169a592802e945435917f75a661821479a
Leon Richter <leon.richter@magenta.de>
2023-06-21 22:25:54 +0200

    changed name from classify, added text to camera

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 trained with  [https://www.kaggle.com/datasets/niharika41298/yoga-poses-dataset/download?datasetVersionNumber=1 ](https://www.kaggle.com/datasets/niharika41298/yoga-poses-dataset/download?datasetVersionNumber=1 )
 
+## Files
+csvwriter.py: input:images in data/TRAIN/ and data/TEST/ output:csv files in csv/train/ and csv/test/ with mediapipe
+classify++.py: trains and tests the model with the csv files in csv/train/ and csv/test/ and saves the model in models/ with the name model.pt
+test_mdl.py : Webcam to see poses live
--- test_mdl.py
+++ test_mdl.py
@@ -1,7 +1,7 @@
 import cv2
 import torch
 from csvwriter import PoseDetector
-from classify import Model
+from classifypp import Model
 
 class ExercisePredictor:
     def __init__(self, model_path):
@@ -9,11 +9,12 @@ class ExercisePredictor:
         self.model = Model()
         self.model.load_state_dict(torch.load(model_path))
         self.model.eval()
+        self.results = None
 
     def preprocess(self, img):
         position = self.detector.findPosition(img)
         flattened_position = []
-        for id, x, y, z in position[0]:
+        for x, y, z in position[0]:
             flattened_position.extend([x, y, z])
         tensor = torch.tensor(flattened_position, dtype=torch.float32)
         tensor = tensor.unsqueeze(0)
@@ -25,14 +26,26 @@ class ExercisePredictor:
         pred_class = prediction.argmax(dim=1)
         return pred_class
 
+    def print_lm_on_screen(self, img, text = None):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.detector.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            mp.solutions.drawing_utils.draw_landmarks(img, self.results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
+            cv2.putText(img, str(text), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+        return img
+
+
 if __name__ == "__main__":
     predictor = ExercisePredictor('model.pt')
     cap = cv2.VideoCapture(0)
+    pTime = 0
+    positions = []
 
-    while True:
+    while cap.isOpened():
         ret, frame = cap.read()
         pred_class = predictor.predict(frame)
-        print(f"Predicted exercise: {pred_class}")
+        frame = predictor.print_lm_on_screen(frame, text=pred_class)
         cv2.imshow('Webcam', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
